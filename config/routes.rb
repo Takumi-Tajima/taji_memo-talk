7710610435ab58986ac5f6ad96259238462b79d5
taji <tajima.takumi0421@gmail.com>
2023-08-30 08:11:43 +0000

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,15 @@
 Rails.application.routes.draw do
-    
-  get 'questions/new'
-  get 'questions/index'
-  get 'questions/edit'
-  get 'questions/show'
-  post 'questions/create'
-  patch 'questions/update'
-  delete 'questions/destroy'
-  get "questions/mypost"
- 
-  
    devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     passwords:'users/passwords'
   }
   
-#   resources :questions do
-#     collection do
-#       get "mypost", to: "questions#mypost"
-#     end
-#   end
-
-# indexを入力するとshowのビューが表示される
+  resources :questions do
+    collection do
+      get "mypost"
+    end
+  end
   
   resources :memos
   root "memos#index"
