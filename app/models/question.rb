e5dfbfa69cceea6ed7c39cf8d512060e567dbe69
Ubuntu <ubuntu@ip-172-31-40-105.ap-northeast-1.compute.internal>
2023-08-31 07:50:54 +0000

    モデルのリファクタリング

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
-  has_many :answers, dependent: :destroy
   belongs_to :user
+  has_many :answers, dependent: :destroy
   
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true
