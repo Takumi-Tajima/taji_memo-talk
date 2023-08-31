d5efb6f37c8257b75358cf09bb236cb4eaac03a9
Ubuntu <ubuntu@ip-172-31-40-105.ap-northeast-1.compute.internal>
2023-08-31 06:36:48 +0000

    answersに変更

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,7 @@
 class Question < ApplicationRecord
-  has_many :answer, dependent: :destroy
+  has_many :answers, dependent: :destroy
   belongs_to :user
   
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true
-
 end
