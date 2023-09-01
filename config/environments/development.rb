72dc7c259a9763d2f11b18c233f7097b169d2954
Ubuntu <ubuntu@ip-172-31-40-105.ap-northeast-1.compute.internal>
2023-09-01 05:57:37 +0000

    パスワードリセットの装飾

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: 'b66f48d2fbaf4f0d877950073b8d5d04.vfs.cloud9.ap-northeast-1.amazonaws.com' }
   config.web_console.whitelisted_ips = '0.0.0.0/0'
   config.hosts.clear
 end
