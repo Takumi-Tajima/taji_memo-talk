d6c650d1c8e5021cf32495ae907648e53b5d080d
Ubuntu <ubuntu@ip-172-31-40-105.ap-northeast-1.compute.internal>
2023-09-04 08:11:05 +0000

    新規ユーザーは追加できないように

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,3 +1,9 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   layout 'admin/application'
+  
+  before_action :redirect_new_action, only: [:new]
+
+  def redirect_new_action
+    redirect_to new_admin_session_path, alert: "新しいユーザーの登録は無効です"
+  end
 end
