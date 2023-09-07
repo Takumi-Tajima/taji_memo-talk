de092f5788549d45bb2ad1e63d62baec09e2a329
taji <tajima.takumi0421@gmail.com>
2023-09-07 06:14:03 +0000

    Fwords置換処理 question.rbに記載

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,5 +5,29 @@ class Question < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true
   
+  before_save :filter_bad_words
+  
   scope :default_order, -> { order(created_at: :desc) }
+  
+  BAD_WORDS = ["死ね", "クソ", "馬鹿", "バカ", "しね", "あほ", "アホ", "キモい", "キモ"].freeze
+
+  private
+
+  def filter_bad_words
+    if title.present?
+      
+      BAD_WORDS.each do |word|
+        title.gsub!(word, '😀' * word.length) if title.include?(word)
+      end
+    end
+
+    if description.present?
+      words = description.split(' ')
+      words.map! do |word|
+        BAD_WORDS.include?(word) ? '😀' * word.length : word
+      end
+
+      self.description = words.join(' ')
+    end
+  end
 end
