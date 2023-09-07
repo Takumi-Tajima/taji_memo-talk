64f16d4e6141884caf0dc9e73a48252ee3cdd89d
nayu <nayu@xa3.so-net.ne.jp>
2023-09-07 04:23:03 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'reactions/create'
-  get 'reactions/destroy'
-
+  
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
     registrations: 'admins/registrations',
